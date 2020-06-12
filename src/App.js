8ed426ef4c3db8db41976c4e6a3bb725007cde3f
hudaman0621 <mrhu828@gmail.com>
2020-06-12 11:41:59 -0400

    somewhat working todo app with delete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; //points to the css bootstrap file
 import './App.css';
 import TodoForm from "./component/TodoForm"
-import { Button, Col, Row } from 'react-bootstrap' //using the Button, Col, Row
+// import { Button, Col, Row } from 'react-bootstrap' //using the Button, Col, Row
 
 class App extends React.Component {
   constructor() {
@@ -12,19 +12,28 @@ class App extends React.Component {
     this.state = {
       todos: [
         {
-          name: 'Todo Item 1',
-          complete: false,
+          name: 'Todo Item I will be here forever when the browser reFreshes', //0
+          complete: false, 
         },
         {
-          name: 'Todo Item the Second',
+          name: 'Todo Item the Second, me too!',    //1
           complete: false,
         },
-        {
-          name: 'The Real todo item',
+        { 
+          name: 'The Real todo item, me 3!',   //2
           complete: true,
         }
       ],
     }
+  }
+
+  removeTodo(indexToRemove) {
+    const filtersTodo = this.state.todos.filter((todoObj, index) => {   //filter, it's going to take a call back function, take each item on the array {obj, obj}, returns a true or false/ if returns false, it will not put in the filter.
+    return this.state.todos[index] !== this.state.todos[indexToRemove] //this statement returns a boolean, it checks each item in the array, if it's not the item we are trying to remove, it puts it into filters todos.
+    })
+    this.setState({
+      todos: filtersTodo //this part, we change the array to an new array that doesn't have removed item. 
+    })
   }
 
   onFormSubmit = (data) => {
@@ -37,16 +46,20 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>The Real todo App</h1>
-        
-        <TodoForm onFormSubmit={this.onFormSubmit} />
-        <ul>
-          { this.state.todos.map ((todo, index) => {
-            return (
-              <li key={index}>{ todo.name } ({ todo.complete ? 'done' : 'not done' })</li>
-            )
-          })}
-        </ul>'
-        </header>'
+      {/* removeTodo and onFOrmSUbmit are the 2 props */}
+          <TodoForm removeTodo={this.removeTodo} onFormSubmit={this.onFormSubmit} /> 
+          <ul>
+            {this.state.todos.map((todo, index) => {
+              return (
+                  <div> 
+                  <li key={index}><p>{todo.name}</p> </li>
+                  <button onClick={this.removeTodo.bind(this, index)}>Remove</button>
+                  </div>
+              )
+            })}
+                  {/* <li onClick={() => { this.removeItem(todo, index) }} key={index}>{todo}</li> */}
+          </ul>
+        </header>
       </div>
 
     );
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -1,50 +1,65 @@
 import React, { Component } from 'react'
+import './style.css';
 
 export default class TodoForm extends Component {
-constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-        name:'',
-        complete: false,
+        this.state = {
+            name: '',
+            complete: false,
+            
+        }
     }
-}
 
-onSubmit = (e) => {
-    e.preventDefault();
-    this.props.onFormSubmit(this.state);
-    this.setState({
-        name: '',
-        complete: false,
-    })
-}
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onFormSubmit(this.state); //getting it from the app.js PROPS
+        this.setState({
+            name: '',
+            complete: false,
+        })
+    }
 
-onNameChange = (e) => {
-    this.setState({
-        name: e.target.value,
-    })
-}
+    onDelete = (e) => { 
+        this.setState({
+            delete: e.target.value,
+        })
+    }
+    onNameChange = (e) => {
+        this.setState({
+            name: e.target.value,
+        })
+    }
 
-onCompleteChange = (e) => {
-    this.setState({
-        complete: e.target.checked
-    })
-}
+    onCompleteChange = (e) => {
+        this.setState({
+            complete: e.target.checked
+        })
+    }
+
+    // removeTodo(e) {
+    //     this.props.removeTodo();  //getting it from the app.js  PROPS
+    // }
+
     render() {
         return (
-            <form onSubmit={this.onSubmit}> 
-            <div>
-                <label htmlFor="name">Name</label>    
-                <input type="text" id="name"
-                    Value= { this.state.name } onChange={this.onNameChange} />
+            <div class="main">
+                <form class="todoform" onSubmit={this.onSubmit}>
+                    <div>
+                        <label htmlFor="name">Enter Missions here =></label>
+                        <input type="text" id="name"
+                            Value={this.state.name} onChange={this.onNameChange} />
+                        <label htmlFor="complete">Completed?</label>
+                        <input type="checkbox" id="complete" value="complete"
+                            checked={this.state.delete} onChange={this.onDelete} />
+                    </div>
+                    <button type="submit">Add</button>
+                    <div>
+                        {/* <button onClick={this.props.removeTodo} type="submit">Delete</button> */}
+                    </div>
+                </form>
             </div>
-            <div>
-                <label htmlFor="complete">Completed?</label>
-                <input type="checkbox" id="complete" value="complete"
-                    checked={ this.state.complete } onChange={this.onCompleteChange} />
-            </div>
-            <button type="submit">Add</button>
-            </form>
         )
     }
 }
